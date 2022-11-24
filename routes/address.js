9b83c7e11c3e61a8f6d0b5951823b7869463ddf6
mohit355 <mohitraj772002@gmail.com>
2022-11-25 04:06:16 +0530

    ss

--- routes/address.js
+++ routes/address.js
@@ -8,6 +8,6 @@ router.get("/address/getByUserId/:userId",auth, getAllAddressByUserId);
 router.get("/address/getById/:id",auth, getAddressById);
 router.post("/address/add",auth, addAddress);
 router.post("/address/update/:id",auth, updateAddress);
-router.post("/address/delete/:id",auth, DeleteAddress);
+router.delete("/address/delete/:id",auth, DeleteAddress);
 
 module.exports = router;
